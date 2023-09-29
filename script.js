cf16a8279b0dfb1d856059cb37fc32fe32ea5b67
Homero Hurtado <homerohg28@gmail.com>
2023-09-29 12:20:29 -0600

    PDF download and margin adjustment

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let div = document.querySelector(".container");
-let btn = document.querySelector(".downloadBtn");
-btn.addEventListener('click', () => {
-    html2pdf().from(div).save()
-
-})
+window.onload = function () {
+    document.getElementById("download")
+        .addEventListener("click", () => {
+            const invoice = this.document.getElementById("invoice");
+            var opt = {
+                margin: -0.15,
+                filename: 'CV-Homero_Hurtado.pdf',
+                image: { type: 'jpeg', quality: 1 },
+                html2canvas: { scale: 2 },
+                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+            };
+            html2pdf().from(invoice).set(opt).save();
+        })
+}
