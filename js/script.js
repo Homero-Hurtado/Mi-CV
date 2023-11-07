db45c04e34818567489ff444e43068a9db2f8023
Homero Hurtado <homerohg28@gmail.com>
2023-11-07 15:32:57 -0600

    pdf size

--- js/script.js
+++ js/script.js
@@ -5,10 +5,15 @@ window.onload = function () {
             var opt = {
                 margin: -0.15,
                 filename: 'CV-Homero_Hurtado.pdf',
-                image: { type: 'jpeg', quality: 1 },
+                image: { type: 'jpeg', quality: 0.98},
+                allowTaint: false,
+                letterRendering: 1,
+                useCORS: true,
+                logging: true,
                 html2canvas: { scale: 2 },
                 jsPDF: { unit: 'in', format: [10, 17.5], orientation: 'portrait' }
             };
             html2pdf().from(invoice).set(opt).save();
-        })
-}
+        })}
+
+
